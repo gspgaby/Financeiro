3df2c58b5d78dc52d1c234765d1a87df58157ca9
gspgaby <gspgaby@gmail.com>
2022-05-09 10:09:38 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+$nome_sistema = 'Financeiro';
+$url_sistema = 'http://localhost/financeiro/';
+$email_adm = 'gspgaby';
+$nome_admin = 'gspgaby@gmail.com';
+
+
+//DADOS PARA O BANCO DADOS
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'financeiro';
+
+ ?>
